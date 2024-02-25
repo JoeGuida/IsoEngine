2b724c87423d2af1cb9b0b220e716af9b02cd8c1
Joe Guida <joeguida@outlook.com>
2024-02-25 05:43:36 -0800

    update example to use GL_RGB over GL_RGBA

--- src/main.cpp
+++ src/main.cpp
@@ -54,7 +54,7 @@ int main() {
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 	if (data) {
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
 		stbi_image_free(data);
 	}
 	else {
